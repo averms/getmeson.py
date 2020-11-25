b703f1fa0d4400c5b458f9ba0a0a19ea440f803f
Aman Verma <amanraoverma@gmail.com>
2020-11-25 01:26:49 -0500

    Refactor and clean up. Download to meson-portable/ to not overwrite by mistake.

--- getmeson.py
+++ getmeson.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-# Download meson to the current directory.
-#
-# Copyright 2019 Aman Verma
+# Copyright © 2019 Aman Verma
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
 import io
 import os
+import shutil
 import subprocess
 import sys
+import tarfile
+import urllib.error
+import urllib.request
+from http.client import HTTPResponse
+from typing import Any, NoReturn
 
 VERSION = "0.55.1"
 # fmt: off
@@ -25,15 +32,15 @@ SHA256 = "3b5741f884e04928bdfa1947467ff06afa6c98e623c25cef75adf71ca39ce080"
 TAR_DIR = "meson-" + VERSION
 
 
-def exists(expectedversion: str) -> bool:
-    mesonbinary = os.path.join("meson", "meson.py")
+def meson_exists(expectedversion: str) -> bool:
+    mesonbinary = os.path.join("meson-portable", "meson.py")
 
     if os.path.isfile(mesonbinary):
         # possibility of an exception here...
         mesonver: str = subprocess.run(
             [mesonbinary, "--version"],
             check=True,
-            # alias for text that is compatible with 3.5
+            # alias for text=True that is compatible with 3.5
             universal_newlines=True,
             stdout=subprocess.PIPE,
         ).stdout.rstrip()
@@ -42,13 +49,10 @@ def exists(expectedversion: str) -> bool:
     return False
 
 
-def gettar(url: str) -> bytes:
-    import urllib.error
-    import urllib.request
-
+def get_tar(url: str) -> bytes:
     try:
         print("Downloading {}...".format(url))
-        page = urllib.request.urlopen(url)
+        page: HTTPResponse = urllib.request.urlopen(url)
         return page.read()
     except urllib.error.HTTPError as e:
         sys.exit("{}\nCheck if {} is up.".format(e.code, url))
@@ -58,20 +62,17 @@ def gettar(url: str) -> bytes:
         page.close()
 
 
-def isvalidhash(file: bytes, expectedhash: str) -> bool:
-    import hashlib
-
+def is_valid_hash(file: bytes, expectedhash: str) -> bool:
     return hashlib.sha256(file).hexdigest() == expectedhash
 
 
-def checkedrename(src: str, dst: str) -> None:
+def checked_rename(src: str, dst: str) -> None:
     if src != TAR_DIR:
         sys.exit(
             "The archive extracted path was unexpected. "
             "Please try to extract it yourself"
         )
     if os.path.exists(dst):
-        import shutil
 
         print("Overwriting {}.".format(dst))
         shutil.rmtree(dst)
@@ -79,37 +80,35 @@ def checkedrename(src: str, dst: str) -> None:
     os.rename(src, dst)
 
 
-def untartodir(tar: bytes) -> None:
-    import tarfile
+def untar_to_dir(tar: bytes) -> None:
+    with io.BytesIO(tar) as tar_io:
+        with tarfile.open(fileobj=tar_io, mode="r") as tf:
+            try:
+                extracteddir = tf.getmembers()[0].name
+                tf.extractall()
+            except tarfile.CompressionError as e:
+                die(e)
 
-    try:
-        tarbuffered: io.BytesIO = io.BytesIO(tar)
-        t = tarfile.open(fileobj=tarbuffered, mode="r")
-        extracteddir = t.getmembers()[0].name
-        t.extractall()
-    except tarfile.CompressionError as e:
-        print(str(e), file=sys.stderr)
-    else:
-        checkedrename(extracteddir, "meson")
-    finally:
-        tarbuffered.close()
-        t.close()
+    checked_rename(extracteddir, "meson-portable")
 
 
-if not exists(VERSION):
+def die(msg: Any) -> NoReturn:
+    print("ERROR:", msg, file=sys.stderr)
+    sys.exit(1)
+
+
+if not meson_exists(VERSION):
     if "-n" in sys.argv[1:]:
         sys.exit(
-            (
-                "Did not find meson-{} and dry run was requested.\n"
-                "Would have installed {}"
-            ).format(VERSION, URL)
+            "Did not find meson-{} and dry run was requested.\n".format(VERSION)
+            + "Would have installed {}".format(URL)
         )
-    mesontar = gettar(URL)
-    if isvalidhash(mesontar, SHA256):
-        untartodir(mesontar)
+    mesontar = get_tar(URL)
+    if is_valid_hash(mesontar, SHA256):
+        untar_to_dir(mesontar)
         print(
-            "Meson should be installed. You can run it with `./meson/meson.py` on *nix"
-            " and `python.exe meson\\meson.py` on Windows"
+            "Meson should be installed. You can run it with `./meson-portable/meson.py` on *nix"
+            " and `python.exe meson-portable\\meson.py` on Windows"
         )
     else:
         sys.exit(
--- make.sh
+++ make.sh
@@ -3,5 +3,5 @@
 set -e
 
 . venv/bin/activate
-strip-hints getmeson.py > getmeson_3.5.py
+strip-hints getmeson.py | sed -E '/from typing import/d' > getmeson_3.5.py
 $VENV_HOME/nvim/bin/black getmeson_3.5.py
